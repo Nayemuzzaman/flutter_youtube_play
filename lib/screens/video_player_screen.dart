4d31c0560308268851d95583f3934e398de6c5c9
nayemuzzaman <nayemzaman@gmail.com>
2022-08-23 04:04:13 +0600

    json data api youtube

---
+++ lib/screens/video_player_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class VideoPlayerScreen extends StatefulWidget {
+  const VideoPlayerScreen({Key? key}) : super(key: key);
+
+  @override
+  State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const String KEY = 'AIzaSyDCDFz1V4FHv38sdW4VCl4H66Z04elTJZE';
+}
